abddea51d116980244c972a6bb16186095251224
cpearam <rambertapolto@gmail.com>
2025-02-12 21:18:19 +0800

    serializers_singletomany

--- api/urls.py
+++ api/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('products/', views.product_list),
+    path('products/<int:pk>/', views.product_detail),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from app.serializers import ProductSerializer
+from app.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    serializers = ProductSerializer(products, many=True)
+    return Response(serializers.data)
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializers = ProductSerializer(product)
+    return Response(serializers.data)
